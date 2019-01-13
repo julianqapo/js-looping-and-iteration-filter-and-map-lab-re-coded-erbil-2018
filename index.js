43c991e2b16e3923da29113e8a31c98b8cf466e7
Julian M. Qapo <julian.qapo@gmail.com>
2019-01-13 12:56:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here:
+function driversWithRevenueOver(driver, r){
+let x =driver.filter(function(v) {if (v.revenue > r){return driver}
+})
+return x
+}
+
+
+function driverNamesWithRevenueOver(d, r){
+  let driverName =[]
+  let x = d.filter(function(v){
+    if (r < v.revenue){
+      driverName.push(v.name)
+    }
+  })
+  return driverName
+}
+
+
+function exactMatch(d, {x:y}){
+let z = d.filter(function(v){
+  if (x.y == v.x){
+    return d
+  }
+})
+return z
+}
